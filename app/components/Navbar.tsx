4729bbedae54cfca5f528ddb4ceb0ee91b5e342f
Blacktrecks <petricaroman815@gmail.com>
2024-11-18 18:48:16 +0200

    feat: Add survey for selecting a exam data

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -96,10 +96,10 @@ export default function Navbar() {
                   <ul className="py-2 text-sm text-gray-700 dark:text-gray-400">
                     <li>
                       <Link
-                        href="/dashboard"
+                        href="/chestionar"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
-                        Dashboard
+                        Chestionar
                       </Link>
                     </li>
                     <li>
