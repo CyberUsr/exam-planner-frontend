eb06c53859fb67b2c74d44b99fd810cd1860502b
Blacktrecks <petricaroman815@gmail.com>
2024-11-23 15:55:23 +0200

    added register page

--- app/services/loginService.jsx
+++ app/services/loginService.jsx
@@ -4,14 +4,20 @@ const AUTH_API_URL = "http://localhost:3003/auth"; // Base URL for the Auth API
 
 // Function to register a new user
 export const registerUser = async (email, password) => {
-  const response = await axios.post(`${AUTH_API_URL}/register`, { email, password });
-  return response.data;
+  const response = await axios.post(`${AUTH_API_URL}/register`, {
+    email,
+    password,
+  });
+  return response.data; // Returns message and user data
 };
 
 // Function to log in a user
 export const loginUser = async (email, password) => {
-  const response = await axios.post(`${AUTH_API_URL}/login`, { email, password });
-  return response.data;
+  const response = await axios.post(`${AUTH_API_URL}/login`, {
+    email,
+    password,
+  });
+  return response.data; // Returns token and user data
 };
 
 // Function to access a protected route
